9aced60558e553538ec292b21bd56b306bc5f059
Auryx <lilbizb1@gmail.com>
2023-06-07 19:13:23 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 // Setup - Import deps and create app object
 ////////////////////////
 const express = require("express")
-
 const app = express()
 //////////////////////
 // Declare Middleware
@@ -19,6 +18,13 @@ app.get("/greeting", (req, res) => {
 app.get("/greeting/:name", (req, res) => {
     console.log(req.params.name) // req.params = url params
     res.send(`Good tidings unto thee, ${req.params.name}`)
+})
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    let total = (req.params.total)
+    let tipPercentage = (req.params.tipPercentage)
+    let final = total * (tipPercentage / 100)
+    res.send(`Your tip will be $${final}`)
 })
 
 ///////////////////////////
